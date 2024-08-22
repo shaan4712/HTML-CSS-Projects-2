90089ab7d044fe591b42c0e9f7e3f3c909623c8a
Shantanu Atmakuri <114588524+shaan4712@users.noreply.github.com>
2024-08-22 16:33:14 +0530

    Add files via upload

---
+++ signapp.js
@@ -0,0 +1,68 @@
+const colorPicker = document.getElementById('colorPicker');
+const canvasColor=document.getElementById('canvasColor');
+const canvas = document.getElementById('myCanvas');
+const clearButton = document.getElementById('clearButton');
+const saveButton = document.getElementById('saveButton');
+const retrieveButton = document.getElementById('retrieveButton');
+const fontSizePicker = document.getElementById('fontSizePicker');
+        
+const ctx = canvas.getContext('2d');
+
+colorPicker.addEventListener('change', (e) =>{
+        ctx.strokeStyle = e.target.value;
+        ctx.fillstyle = e.target.value;
+    })
+
+canvas.addEventListener('mousedown', (e)=>{
+        isDrawing = true;
+        lastX = e.offsetX;
+        lastY = e.offsetY;
+})
+
+canvas.addEventListener('mousemove', (e)=>{
+        if (isDrawing){
+                ctx.beginPath();
+                ctx.moveTo(lastX, lastY);
+                ctx.lineTo(e.offsetX, e.offsetY);
+                ctx.stroke();
+
+                lastX = e.offsetX;
+                lastY = e.offsetY; //changes each time mouse moves
+        }
+})
+
+canvas.addEventListener('mouseup', (e)=>{
+       isDrawing = false; 
+})
+
+canvasColor.addEventListener('change', (e)=>{
+        ctx.fillStyle = e.target.value;
+        ctx.fillRect(0,0,800,500);
+})
+
+fontSizePicker.addEventListener('change', (e)=>{
+        ctx.lineWidth = e.target.value;
+})
+
+clearButton.addEventListener('click', (e)=>{
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+})
+
+saveButton.addEventListener('click', ()=>{
+        localStorage.setItem('canvasContents', canvas.toDataURL());
+
+        let link = document.createElement('a');
+        link.download = 'my-canvas.png';
+        link.href = canvas.toDataURL();
+        link.click();
+})
+
+retrieveButton.addEventListener('click', ()=>{
+        let savedCanvas = localStorage.getItem('canvasContents');
+
+        if (savedCanvas){
+                let img = new Image();
+                img.src = savedCanvas;
+                ctx.drawImage(img, 0, 0);
+        }
+})
